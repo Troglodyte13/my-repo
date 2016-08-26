e070b8aaf500e3a5574987eaaca1ebec75ac5ac8
Kelly Lanier <klanier@stumail.fhtc.edu>
2016-08-26 09:54:12 -0500

    Initial project

---
+++ TestingGit/src/Program.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Program {
+	public static Scanner in = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		in.close();
+	}
+
+}
